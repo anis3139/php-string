e4c839bb75605513ec9737676027e95874266de8
anis904692@gmail.com <anis904692@gmail.com>
2020-07-08 17:48:07 +0600

    String complete

--- string.php
+++ string.php
@@ -69,15 +69,87 @@ echo $string[$i*-1];
 
 $string= "Hello World,Rakib, How are you";
 
-$parts=explode(' ',$string);
-print_r($parts);
+// $parts=explode(' ',$string);
+// print_r($parts);
 
-$original=implode(' ',$parts); //implode=join
-print_r($original);
-
-
+// $original=implode(' ',$parts); //implode=join
+// print_r($original);
 
 
+// $parts2=preg_split("/ |,/",$string);
+// print_r($parts2);
+
+
+// $parts3=strtok($string, " ,");
+// while($parts3 !== false){
+//     echo $parts3."\n";
+// $parts3=strtok(" ,");
+// }
+
+
+
+
+
+
+
+
+
+// search in string
+
+// $string="Hello World How Are You. How";
+
+// //echo strpos($string,'How',13);
+// $word="how";
+// $offset= stripos($string,$word);
+
+// if($offset!==false){
+//     echo "'{$word}' is {$offset} possition in the sentense";
+// }else{
+    
+//     echo "result not found";
+
+// }
+
+
+
+
+//Trim
+
+// $string= " hello \n";
+// $string= trim($string, ' \n');//rtrim, ltrim
+// echo $string;
+// echo "data";
+
+
+//wordwrap
+
+// $string="lorem ipsum dollqar emmet trap hello world in dhaka to home it may concern dkjfn jisdjfo jnjcsd jdshsjkddoik jadnjs";
+// echo wordwrap($string,10,"\n",true);    //<br/> for webpage
+
+
+
+
+
+
+//new line brack
+
+// $string="lorem ipsum dollqar emmet trap hello world\n in dhaka to home it may concern\n dkjfn jisdjfo jnjcsd jdshsjkddoik\n jadnjs";
+// echo nl2br($string);
+
+
+
+
+//scanf
+
+
+// $string="Abul Hossain 01816366535 anis904692@gmail.com";
+
+// $parts=sscanf($string, "%s %s %11s %s");
+// print_r($parts);
+
+
+// sscanf($string, "%s %s %11s %s", $fname, $lname, $mobile, $email);
+// echo $email;
 
 
 
